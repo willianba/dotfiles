b91e670d51a6bf84171b1da0c689fcc2474958e3
Willian Alves <wba.alves@gmail.com>
2022-06-03 20:07:20 -0300

    remove old dotfiles

--- powershell/powershell.dotfile.ps1
+++
@@ -1,8 +0,0 @@
-# Install modules
-Install-Module posh-git -Scope CurrentUser
-Install-Module oh-my-posh -Scope CurrentUser
-Install-Module -Name PSReadLine -AllowPrerelease -Scope CurrentUser -Force -SkipPublisherCheck
-Install-Module -Name Terminal-Icons -Repository PSGallery
-
-# Copy profile file - Adapt to match Documents folder
-Copy-Item .\powershell.profile.ps1 ~\Documents\Powershell\Microsoft.PowerShell_profile.ps1
--- ubuntu/.zshrc
+++
@@ -1,8 +0,0 @@
-export ZSH="/home/$USER/.oh-my-zsh"
-ZSH_THEME="powerlevel10k/powerlevel10k"
-ZSH_DISABLE_COMPFIX=true
-plugins=(git fast-syntax-highlighting zsh-autosuggestions)
-source $ZSH/oh-my-zsh.sh
-# User configuration
-export LANG=pt_BR.UTF-8
-export DISPLAY=:0
