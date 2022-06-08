34dd2abd2150044684db1d53c17d31a0a4e28f89
Willian Alves <willian.alves@sap.com>
2022-06-08 13:57:24 -0300

    rename config file

--- nvim/plugins/overrides.lua
+++ nvim/plugins/overrides.lua
@@ -56,4 +56,19 @@ M.telescope = {
   extensions_list = { "themes", "terms", "workspaces" }
 }
 
+M.nvterm = {
+  terminals = {
+    type_opts = {
+      float = {
+        relative = "editor",
+        row = 0.1,
+        col = 0.1,
+        width = 0.8,
+        height = 0.8,
+        border = "single",
+      }
+    }
+  }
+}
+
 return M
