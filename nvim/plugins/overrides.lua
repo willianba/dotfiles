930dd88af045c86c8ee6f264eb1f38eb611fa883
Willian Alves <willian.alves@sap.com>
2022-07-15 16:33:01 -0300

    add cmp overrides | format file

--- nvim/plugins/overrides.lua
+++ nvim/plugins/overrides.lua
@@ -1,76 +1,86 @@
 -- this file tracks plugins configs to override/add
---
+
 local M = {}
 
 M.nvimtree = {
-	git = {
-		enable = true,
-	},
-	view = {
-		adaptive_size = true,
-		width = 35,
-	},
-	renderer = {
-		group_empty = true,
-		highlight_git = true,
-		icons = {
-			show = {
-				git = true,
-			},
-		},
-	},
-	filters = {
-		dotfiles = false,
-	},
-	diagnostics = {
-		enable = false,
-		show_on_dirs = true,
-	},
+  git = {
+    enable = true,
+  },
+  view = {
+    adaptive_size = true,
+    width = 35,
+  },
+  renderer = {
+    group_empty = true,
+    highlight_git = true,
+    icons = {
+      show = {
+        git = true,
+      },
+    },
+  },
+  filters = {
+    dotfiles = false,
+  },
+  diagnostics = {
+    enable = false,
+    show_on_dirs = true,
+  },
 }
 
 M.treesitter = {
-	ensure_installed = {
-		"lua",
-		"vim",
-		"typescript",
-		"javascript",
-		"json",
-		"markdown",
-		"bash",
-		"html",
-		"css",
-		"ruby",
-		"go",
-		"graphql",
-		"prisma",
-	},
-	rainbow = {
-		enable = true,
-		extended_mode = true, -- Also highlight non-bracket delimiters like html tags, boolean or table: lang -> boolean
-		max_file_lines = nil, -- Do not enable for files with more than n lines, int
-	},
-	autotag = {
-		enable = true,
-	},
+  ensure_installed = {
+    "lua",
+    "vim",
+    "typescript",
+    "javascript",
+    "json",
+    "markdown",
+    "bash",
+    "html",
+    "css",
+    "ruby",
+    "go",
+    "graphql",
+    "prisma",
+  },
+  rainbow = {
+    enable = true,
+    extended_mode = true, -- Also highlight non-bracket delimiters like html tags, boolean or table: lang -> boolean
+    max_file_lines = nil, -- Do not enable for files with more than n lines, int
+  },
+  autotag = {
+    enable = true,
+  },
 }
 
 M.telescope = {
-	extensions_list = { "themes", "terms", "workspaces" },
+  extensions_list = { "themes", "terms", "workspaces" },
 }
 
 M.nvterm = {
-	terminals = {
-		type_opts = {
-			float = {
-				relative = "editor",
-				row = 0.1,
-				col = 0.1,
-				width = 0.8,
-				height = 0.8,
-				border = "single",
-			},
-		},
-	},
+  terminals = {
+    type_opts = {
+      float = {
+        relative = "editor",
+        row = 0.1,
+        col = 0.1,
+        width = 0.8,
+        height = 0.8,
+        border = "single",
+      },
+    },
+  },
+}
+
+M.cmp = {
+  sources = {
+    { name = "nvim_lsp" },
+    { name = "luasnip" },
+    { name = "buffer" },
+    { name = "nvim_lua" },
+    { name = "path" },
+  },
 }
 
 return M
