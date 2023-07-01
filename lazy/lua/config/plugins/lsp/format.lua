61cb3fbe2db41fc422836d42913fde04293a1f53
Willian Alves <wba.alves@gmail.com>
2023-07-01 19:16:11 -0300

    move autoformat to the util file

--- lazy/lua/config/plugins/lsp/format.lua
+++ lazy/lua/config/plugins/lsp/format.lua
@@ -1,11 +1,6 @@
+local util = require("config.settings.util")
+
 local M = {}
-
-M.autoformat = true
-
-function M.toggle()
-  M.autoformat = not M.autoformat
-  vim.notify(M.autoformat and "Enabled format on save" or "Disabled format on save")
-end
 
 function M.format()
   local buf = vim.api.nvim_get_current_buf()
@@ -29,7 +24,7 @@ function M.on_attach(client, buf)
       group = vim.api.nvim_create_augroup("LspFormat." .. buf, {}),
       buffer = buf,
       callback = function()
-        if M.autoformat then
+        if util.autoformat then
           M.format()
         end
       end,
--- lazy/lua/config/settings/util.lua
+++ lazy/lua/config/settings/util.lua
@@ -1,5 +1,12 @@
 local M = {}
 
+M.autoformat = true
+
+M.toggle_autoformat = function()
+  M.autoformat = not M.autoformat
+  vim.notify(M.autoformat and "Enabled format on save" or "Disabled format on save")
+end
+
 M.toggle_dark_mode = function()
   local theme_color = vim.o.background == "dark" and "light" or "dark"
   vim.o.background = theme_color
