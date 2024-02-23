973842d4f456ebc034b26b5755645a21f7699b67
Willian Alves <wba.alves@gmail.com>
2024-02-23 15:36:31 -0300

    fix visual comment

--- lazy/lua/config/plugins/mini.lua
+++ lazy/lua/config/plugins/mini.lua
@@ -14,6 +14,7 @@ return {
         mappings = {
           comment = "<leader>/",
           comment_line = "<leader>/",
+          comment_visual = "<leader>/",
           textobject = "<leader>/",
         },
       })
