707b0daf6c03474ec7beb0dfff3a272dea2e1048
Willian Alves <wba.alves@gmail.com>
2024-03-25 22:43:03 -0300

    add some missing packages

--- install.sh
+++ install.sh
@@ -16,23 +16,22 @@ chsh -s $(which zsh)
 
 echo "Install oh-my-zsh plugins"
 git clone https://github.com/zsh-users/zsh-autosuggestions $OHMYZSH_CUSTOM_PATH/plugins/zsh-autosuggestions
-git clone https://github.com/zdharma/fast-syntax-highlighting.git $OHMYZSH_CUSTOM_PATH/plugins/fast-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $OHMYZSH_CUSTOM_PATH/plugins/zsh-syntax-highlighting
 
 echo "Install apps"
 brew install docker --cask
-brew install neovim ripgrep fd
+brew install neovim ripgrep fd fzf
 
-echo "Install NvChad"
-git clone https://github.com/NvChad/NvChad $NVIM_PATH --depth 1
-mkdir $NVIM_PATH/lua/custom
+echo "Create neovim config folder"
+mkdir -r $NVIM_PATH
 
 echo "Clone repo"
 git clone https://github.com/willianba/dotfiles.git $DOTFILES_PATH
 
 echo "Set config files"
 ln -sf $DOTFILES_PATH/.gitconfig $HOME
-ln -sf $DOTFILES_PATH/zsh/.zshrc $HOME
-ln -sf $DOTFILES_PATH/zsh/*.zsh $OHMYZSH_CUSTOM_PATH
-ln -sf $DOTFILES_PATH/nvim/* $NVIM_PATH/lua/custom/
+# ln -sf $DOTFILES_PATH/zsh/.zshrc $HOME
+# ln -sf $DOTFILES_PATH/zsh/*.zsh $OHMYZSH_CUSTOM_PATH
+ln -sf $DOTFILES_PATH/lazy/* $NVIM_PATH
 
 zsh
