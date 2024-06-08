fa80f102b069463a286a063d5f7c1755f6a1a886
Willian Alves <wba.alves@gmail.com>
2024-06-07 23:11:34 -0300

    make oil show hidden files

--- lazy/lua/config/plugins/oil.lua
+++ lazy/lua/config/plugins/oil.lua
@@ -1,7 +1,14 @@
 return {
   "stevearc/oil.nvim",
   dependencies = { "nvim-tree/nvim-web-devicons" },
-  config = true,
+  config = function()
+    require("oil").setup({
+      delete_to_trash = true,
+      view_options = {
+        show_hidden = true,
+      },
+    })
+  end,
   keys = {
     { "<C-n>", "<cmd>Oil<cr>" },
   },
