8158dc80c4206d79acf7b382e183b0bf91aef342
Willian Alves <wba.alves@gmail.com>
2024-06-24 11:56:20 -0300

    remove trouble

--- lazy/lua/config/plugins/trouble.lua
+++
@@ -1,9 +0,0 @@
-return {
-  "folke/trouble.nvim",
-  enable = false,
-  cmd = { "Trouble", "TroubleToggle" },
-  keys = {
-    { "<leader>tr", "<cmd>TroubleToggle<cr>" },
-  },
-  dependencies = { "nvim-tree/nvim-web-devicons" },
-}
