b4035c25e9f889ecfe2aab0a0e8d0a9017de8bae
Willian Alves <wba.alves@gmail.com>
2026-01-06 10:44:33 -0300

    fix indent

--- dot_config/fish/functions/release_commits.fish
+++ dot_config/fish/functions/release_commits.fish
@@ -1,3 +1,4 @@
 function release_commits
-    git log origin/production..(git rev-parse --abbrev-ref HEAD) --format='%h - %ad - %s %aN' --date=format:'%b %d %Y' | tail -r | sed 's/^/- /'
+  git log origin/production..(git rev-parse --abbrev-ref HEAD) --format='%h - %ad - %s %aN' --date=format:'%b %d %Y' | tail -r | sed 's/^/- /'
 end
+
