67202d9534e3147d495f17ee9cdde0de62229632
Willian Alves <wba.alves@gmail.com>
2026-03-01 21:22:22 -0300

    fix fish theme

--- dot_config/fish/functions/set_fish_theme.fish
+++ dot_config/fish/functions/set_fish_theme.fish
@@ -1,10 +1,6 @@
 function set_fish_theme
-    set current_hour (date +%H)
-
-    if test $current_hour -ge 6 -a $current_hour -lt 18
-        yes | fish_config theme save "Catppuccin Latte"
-    else
-        yes | fish_config theme save "Catppuccin Macchiato"
-    end
+  if status is-interactive
+      fish_config theme choose "Catppuccin Macchiato"
+  end
 end
 
