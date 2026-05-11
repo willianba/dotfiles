faaa05469292d46ecba8e14e2373fdfb21fc1508
Willian Alves <wba.alves@gmail.com>
2026-05-11 00:04:27 -0300

    test: use new treesitter config

--- dot_config/nvim/lua/plugins/treesitter.lua
+++ dot_config/nvim/lua/plugins/treesitter.lua
@@ -15,7 +15,8 @@ return {
     },
   },
   config = function()
-    local ensureInstalled = {
+    local autocmd = vim.api.nvim_create_autocmd
+    local ensure_installed = {
       "bash",
       "css",
       "go",
@@ -31,13 +32,40 @@ return {
       "yaml",
       "vim",
     }
-    local alreadyInstalled = require("nvim-treesitter.config").get_installed()
-    local parsersToInstall = vim
-      .iter(ensureInstalled)
+
+    local already_installed = require("nvim-treesitter.config").get_installed()
+    local parsers_to_install = vim
+      .iter(ensure_installed)
       :filter(function(parser)
-        return not vim.tbl_contains(alreadyInstalled, parser)
+        return not vim.tbl_contains(already_installed, parser)
       end)
       :totable()
-    require("nvim-treesitter").install(parsersToInstall)
+
+    require("nvim-treesitter").setup({
+      install_dir = vim.fn.stdpath("data") .. "/site",
+    })
+
+    require("nvim-treesitter").install(parsers_to_install)
+    require("nvim-ts-autotag").setup()
+
+    local group = vim.api.nvim_create_augroup("user_treesitter", { clear = true })
+    autocmd("FileType", {
+      group = group,
+      callback = function(event)
+        local bufnr = event.buf
+        local filetype = vim.bo[bufnr].filetype
+        if filetype == "" or not vim.treesitter.language.add then
+          return
+        end
+
+        local has_parser = pcall(vim.treesitter.language.add, filetype)
+        if not has_parser then
+          return
+        end
+
+        pcall(vim.treesitter.start, bufnr)
+        vim.bo[bufnr].indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+      end,
+    })
   end,
 }
